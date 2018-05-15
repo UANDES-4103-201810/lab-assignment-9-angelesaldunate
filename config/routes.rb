7f579b84bafaef79ca0c7f98251c841aaac0b041
Angeles <adaldunate@miuandes.cl>
2018-05-15 09:01:09 -0400

    first db done

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :movies
+  resources :addresses
+  resources :categories
+  resources :actors
+  resources :directors
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
